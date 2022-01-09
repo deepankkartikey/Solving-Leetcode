a30fc6cbfa1c94ab6cc9641451959f6252a7e127
Deepank Kartikey <deepankkartikey@gmail.com>
2022-01-09 13:28:27 -0500

    Update 374-guess-number-higher-or-lower.java
    comments

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
@@ -10,12 +10,16 @@
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int start = 1, end = n;
+        // using binary search to look for eaxact answer
         while(start <= end){
             int mid = start + (end-start)/2;
+            // if current number is greater, look into first part
             if(guess(mid) == -1)
                 end = mid-1;
+            // if current number is lesser, look into second part
             else if(guess(mid) == 1)
                     start = mid+1;
+            // found exact number, return answer
             else if(guess(mid) == 0)
                     return mid;
         }
