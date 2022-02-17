acc2b473c2aa4e9776fb59b852ccb9415de0d5c0
Deepank Kartikey <deepankkartikey@gmail.com>
2022-02-17 12:38:57 -0500

    Time: 3 ms (85.41%), Space: 127.9 MB (19.06%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,23 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         // apply cyclic sort
-        // while sorting, neglect -ve numbers ,
-        // and numbers greater than n (i.e. array length)
-        int idx = 0;
+        // neglect -ve numbers and numbers greater than n (=array.length), when sorting
+        int idx=0;
         while(idx < nums.length){
             int correctIdx = nums[idx]-1;
-            if(nums[idx] > 0 && nums[idx] <= nums.length && nums[idx] != nums[correctIdx])
+            if(nums[idx]>0 && nums[idx]<nums.length && nums[idx]!=nums[correctIdx])
                 swap(nums, idx, correctIdx);
-            else 
-                idx ++;
+            else
+                idx++;
         }
         
-        // after appying cyclic sort, the first wrong (index, element) combination points to the missing number
-        for(int i=0; i<nums.length; i++){
+        // check which (index, element) combination is wrong at first
+        // if no such combination found last element is missing
+        for(int i=0; i<nums.length; i++)
             if(nums[i] != i+1)
                 return i+1;
-        }
-        // if no element was missing in middle, last (element==n) is answer
+        
         return nums.length+1;
     }
     
