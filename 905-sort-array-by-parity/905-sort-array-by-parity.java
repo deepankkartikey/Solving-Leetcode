5d0aa60dd28f1428ceb536bfff2a8656714b888c
Deepank Kartikey <deepankkartikey@gmail.com>
2022-05-02 13:01:05 -0400

    Time: 1 ms (96.61%), Space: 48.3 MB (51.15%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         // traverse array
-        // check if num is even or odd
-        // if even put in left part else put in right part
-        int[] res = new int[nums.length];
-        int start = 0, end = nums.length-1;
-        for(int num: nums){
-            if(num%2 == 0)
-                res[start++] = num;
-            else 
-                res[end--] = num;
+        // keep an additional index variable,
+        // and keep track of even numbers
+        // Keep swapping only even numbers with this index
+        int sortedIdx = 0;
+        for(int idx=0; idx<nums.length; idx++){
+            if(nums[idx]%2 == 0){
+                int temp = nums[idx];
+                nums[idx] = nums[sortedIdx];
+                nums[sortedIdx] = temp;
+                sortedIdx++;
+            }
         }
-        return res;
+        return nums;
     }
 }
