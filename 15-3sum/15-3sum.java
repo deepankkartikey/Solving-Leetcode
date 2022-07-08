1470bdd5e8c98addd9817d092f3a4023f8d30630
Deepank Kartikey <deepankkartikey@gmail.com>
2022-07-08 04:15:08 -0400

    Time: 40 ms (41.74%), Space: 58.5 MB (73.76%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for(int i=0; i<nums.length-2; i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int st=i+1;
-                int end=nums.length-1;
-                int target = -nums[i];
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++) {
+            List<Integer> sol = null;
+            if(i==0 || (i>0 && nums[i] != nums[i-1]) ){
+                int st = i+1;
+                int end = nums.length-1;
+                int target = -(nums[i]);
                 while(st < end){
                     if(nums[st]+nums[end] == target){
-                        res.add(Arrays.asList(nums[st], nums[end], nums[i]));
+                        sol = Arrays.asList(nums[st], nums[end], nums[i]);
+                        res.add(sol);
                         while(st<end && nums[st]==nums[st+1]) st++;
-                        while(end>st && nums[end]==nums[end-1])end--;
+                        while(st<end && nums[end-1]==nums[end]) end--;
                         st++;
-                        end--;
-                    } else if(nums[st]+nums[end] < target){
-                        st++;
-                    }else{
                         end--;
                     }
+                    else if(nums[st]+nums[end] < target)
+                        st++;
+                    else
+                        end--;
                 }
             }
         }
