c9a3ec61a8894d8838d45764ad2584e6783b5d2c
Deepank Kartikey <deepankkartikey@gmail.com>
2022-07-17 04:38:09 -0400

    Time: 8 ms (65.00%), Space: 44.5 MB (12.50%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -1,25 +1,26 @@
 class Solution {
+    private Long[][][] dp = null;
+    
+    private long dfs(int m, int n, int maxMove, int r, int c){
+        if(r<0 || r>=m || c<0 || c>=n)
+            return 1;
+        if(maxMove <= 0)
+            return 0;
+        if(dp[r][c][maxMove] != null)
+            return dp[r][c][maxMove];
+        
+        long res = 0;
+        long leftStep = dfs(m, n, maxMove-1, r, c-1);
+        long rightStep = dfs(m, n, maxMove-1, r, c+1);
+        long upStep = dfs(m, n, maxMove-1, r-1, c);
+        long downStep = dfs(m, n, maxMove-1, r+1, c);
+        res = (leftStep + rightStep + upStep + downStep)%1000000007L;
+        dp[r][c][maxMove] = res;
+        return res;
+    }
+    
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        if (maxMove == 0) return 0;
-        int[][] dpCurr = new int[m+2][n+2], dpLast = new int[m+2][n+2];
-        for (int i = 1; i <= m; i++) {
-            dpCurr[i][1]++;
-            dpCurr[i][n]++;
-        }
-        for (int j = 1; j <= n; j++) {
-            dpCurr[1][j]++;
-            dpCurr[m][j]++;
-        }
-        int ans = dpCurr[startRow+1][startColumn+1];
-        for (int d = 1; d < maxMove; d++) {
-            int[][] temp = dpCurr;
-            dpCurr = dpLast;
-            dpLast = temp;
-            for (int i = 1; i <= m; i++)
-                for (int j = 1; j <= n; j++)
-                    dpCurr[i][j] = (int)(((long)dpLast[i-1][j] + dpLast[i+1][j] + dpLast[i][j-1] + dpLast[i][j+1]) % 1000000007L);
-            ans = (ans + dpCurr[startRow+1][startColumn+1]) % 1000000007;
-        }
-        return ans;
+        dp = new Long[m+1][n+1][maxMove+1];
+        return (int)dfs(m, n, maxMove, startRow, startColumn);
     }
 }
