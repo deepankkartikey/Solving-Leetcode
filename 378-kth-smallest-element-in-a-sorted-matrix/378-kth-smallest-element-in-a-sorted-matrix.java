b9eb5e25efe8742bc221e15f3c90b67b86a4b2e1
Deepank Kartikey <deepankkartikey@gmail.com>
2022-08-02 08:45:23 -0400

    Time: 36 ms (23.08%), Space: 56.6 MB (64.41%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,5 +1,6 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
+        // maxHeap using priority queue
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
