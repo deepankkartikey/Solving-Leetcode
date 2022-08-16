5a5f9a291189ce68f4002eaeb3a18a8ac16226cf
Deepank Kartikey <deepankkartikey@gmail.com>
2022-08-16 05:46:12 -0400

    Time: 2 ms (90.88%), Space: 42.2 MB (43.70%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-        for (char it : s.toCharArray()) {
-            if (it == '(' || it == '[' || it == '{')
-                st.push(it);
-            else {
-                if(st.isEmpty()) return false;
-                char ch = st.pop(); 
-                if((it == ')' && ch == '(') ||  
-                   (it == ']' && ch == '[') || 
-                   (it == '}' && ch == '{')) 
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char tos = st.pop();
+                if((ch==')' && tos=='(') ||
+                    (ch==']' && tos=='[') ||
+                    (ch=='}' && tos=='{'))
                     continue;
-                else 
+                else
                     return false;
             }
         }
