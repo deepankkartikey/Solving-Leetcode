602db19ed16fdc90868fb2d87e233ed5aa4b32f0
Deepank Kartikey <deepankkartikey@gmail.com>
2022-08-15 20:22:27 -0400

    Time: 66 ms (37.30%), Space: 44.5 MB (79.71%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // using a hashmap to store indices for each element
-        Map<Integer, Integer> hm = new HashMap<>();
-        
-        // check if cuurrent number and target difference present in hashset
-        // if yes, return indices
-        // if no, add current element to Map and move on
-        for(int idx=0; idx<nums.length; idx++){
-            if(hm.containsKey(target-nums[idx]))
-                return new int[]{idx, hm.get(target-nums[idx])};
-            else
-                hm.put(nums[idx], idx);
+        int[] ans = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
         }
-        return new int[]{0,0};
+        return ans;
     }
 }
