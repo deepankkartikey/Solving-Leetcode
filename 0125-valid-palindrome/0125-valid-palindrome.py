4ad2b09dbbc50133f6acb301e03a62a95eb6cced
Deepank Kartikey <deepankkartikey@gmail.com>
2022-11-01 23:16:43 -0400

    Time: 182 ms (33.20%), Space: 14.2 MB (72.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,27 @@
 class Solution(object):
+    
+    def alphaNum(self, ch):
+        return ((ord('A') <= ord(ch) <= ord('Z')) or
+               (ord('a') <= ord(ch) <= ord('z')) or
+                (ord('0') <= ord(ch) <= ord('9')))
+    
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        newStr = [ch.lower() for ch in s if ch.isalnum()]
-        return newStr == newStr[::-1]
+        l, r = 0, len(s)-1
+        
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l = l+1
+            while l < r and not self.alphaNum(s[r]):
+                r = r-1
+                
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l = l+1
+            r = r-1
+        
+        return True
